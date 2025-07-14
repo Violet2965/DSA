1cc2856b95956d66e298e1ecf314f29389072003
Violet2965 <shivanidevanekar@gmail.com>
2025-07-14 21:01:55 +0530

    dsa day 2 pattern

---
+++ Java/pattern.java
@@ -0,0 +1,29 @@
+class pattern {
+    public static void main(String[] args) {
+        pattern1(5);
+        pattern2(4);
+
+    }
+
+    static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++)
+                System.out.print(" ");
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
