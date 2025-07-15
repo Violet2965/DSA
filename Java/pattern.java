72d41345706f72a6d287e4c2a9f336154399dae5
Violet2965 <shivanidevanekar@gmail.com>
2025-07-15 22:29:48 +0530

    dsa day 3 pattern

--- Java/pattern.java
+++ Java/pattern.java
@@ -2,6 +2,7 @@ class pattern {
     public static void main(String[] args) {
         pattern1(5);
         pattern2(4);
+        pattern3(2);
 
     }
 
@@ -25,5 +26,25 @@ class pattern {
         }
     }
 
-    
+    static void pattern3(int n) {
+        for (int i = 1; i <= n - i + 1; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
 }
