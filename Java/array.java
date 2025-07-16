81504b1c8d1186367c7ffa3a66092b66b56bcb54
Violet2965 <shivanidevanekar@gmail.com>
2025-07-16 22:04:22 +0530

    dsa day 3 pattern

--- Java/array.java
+++ Java/array.java
@@ -2,6 +2,7 @@ class aarray{
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40};
         System.out.println(arr[2]); 
+        System.out.println(+arr.length);
     }  
     
     
